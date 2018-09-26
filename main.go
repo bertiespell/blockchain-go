9ded811a3cf00edef18455232d18b006beee1b06
Bertie <berniesnell@gmail.com>
2018-09-26 13:26:33 +0100

    Add main blockchain go file

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+}
