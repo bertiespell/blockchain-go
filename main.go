bc06c587ce10b92dd88bc8f8f3ba042f64d539af
Bertie <berniesnell@gmail.com>
2018-09-28 10:33:46 +0100

    check incoming chains

--- main.go
+++ main.go
@@ -313,7 +313,21 @@ func readData(rw *bufio.ReadWriter) {
 		}
 		if str != "\n" {
 			chain := make([]Block, 0)
-			if err := json.Unmarshal([]byte(str), &chain);  
+			if err := json.Unmarshal([]byte(str), &chain); err != nil {
+				log.Fatal(err)
+			}
+			mutex.Lock()
+			if len(chain) > len(Blockchain) {
+				Blockchain = chain
+				bytes, err := json.MarshalIndent(Blockchain, "", "	")
+				if err != nil {
+					log.Fatal(err)
+				}
+				// Green console color: 	\x1b[32m
+				// Reset console color: 	\x1b[0m
+				fmt.Printf("\x1b[32m%s\x1b[0m> ", string(bytes))
+			}
+			mutex.Unlock()
 		}
 	}
 }
