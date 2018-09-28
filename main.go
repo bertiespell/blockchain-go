b0f1d9c1736d683135418859f0fc96b52b3f5d51
Bertie <berniesnell@gmail.com>
2018-09-28 14:37:13 +0100

    write data and append to blockchain

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -349,4 +350,36 @@ func writeData(rw *bufio.ReadWriter) {
 			mutex.Unlock()
 		}
 	}()
+
+	stdReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf(">	")
+		sendData, err := stdReader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		sendData = strings.Replace(sendData, "\n", "", -1)
+		bpm, err := strconv.Atoi(sendData)
+		if err != nil {
+			log.Fatal(err)
+		}
+		newBlock := generateBlock(Blockchain[len(Blockchain)-1], bpm)
+		if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+			mutex.Lock()
+			Blockchain = append(Blockchain, newBlock)
+			mutex.Unlock()
+		}
+
+		bytes, err := json.Marshal(Blockchain)
+		if err != nil {
+			log.Println(err)
+		}
+		spew.Dump(Blockchain)
+
+		mutex.Lock()
+		rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
+		rw.Flush()
+		mutex.Unlock()
+	}
 }
